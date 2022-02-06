4fe1b8b5740aaa89f44e717b63eba66a0c54c5cc
Mohit Rathore <80819747+mr-mohit-rathore@users.noreply.github.com>
2022-02-07 01:05:41 +0530

    SolveMeFirstQuestionOnHackerranck

---
+++ SolveMeFirst.java
@@ -0,0 +1,8 @@
+static int solveMeFirst(int a, int b) {
+    
+return (a+b);
+   }
+
+// just function solved
+// question link on sheet and i am also providing here
+// link :- https://www.hackerrank.com/challenges/solve-me-first/problem
